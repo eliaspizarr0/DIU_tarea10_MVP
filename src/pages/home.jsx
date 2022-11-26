15015aca725a9b20a4f1dd3b57bcf16684477f40
Elias <elias.pizarroa@sansano.usm.cl>
2022-11-26 19:00:41 -0300

    act home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import NavBar from '../components/nav_bar'
 import LogoUSM from '../assets/rree.png'
+import imagenact from '../assets/act.png'
 
 export const HomePage = () => {
   return (
     <div className='page'>
       <img src={LogoUSM} height={100} alt='logo rree' /><br />
       <NavBar />
+      <img src={imagenact} height={380} alt='logo act' /><br />
       <h2>Página de Inicio</h2>
     </div>
   )
